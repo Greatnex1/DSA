7add6ceff8d9345d3a3daa09a016ea5133d1522a
Nouah <noahgreatakoni@gmail.com>
2023-03-27 15:32:53 +0100

    dsa test

---
+++ test/HackerRank/ArraySumTest.java
@@ -0,0 +1,16 @@
+package HackerRank;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ArraySumTest {
+    @Test
+    void  arraySum(){
+        ArraySum arry = new ArraySum();
+        int num[] ={1,4,5,6,7,8};
+        arry.arrSum(num);
+
+    }
+
+}
