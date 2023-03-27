ce16d23c1dee4d3be086eb5e7884342570e4744b
Nouah <noahgreatakoni@gmail.com>
2023-03-27 15:34:27 +0100

    dsa_test

---
+++ test/Leetcode/NoRepetitionOfCharacterTest.java
@@ -0,0 +1,15 @@
+package Leetcode;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NoRepetitionOfCharacterTest {
+
+    @Test
+    void lengthOfLongestSubstring() {
+        NoRepetitionOfCharacter noRep = new NoRepetitionOfCharacter();
+
+        noRep.lengthOfLongestSubstring("swallow");
+    }
+}
